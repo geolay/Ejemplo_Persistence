6e555f5ece16ffa4fcf02952ca6eb2d69e1ce27e
ernes <ernesto.olavarria@gmail.com>
2021-01-27 09:57:29 -0300

    Boton Borrar funcionando

--- app/src/main/java/com/example/ejemplo_persistence/FirstFragment.kt
+++ app/src/main/java/com/example/ejemplo_persistence/FirstFragment.kt
@@ -8,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.ejemplo_persistence.databinding.FragmentFirstBinding
 
@@ -31,14 +32,26 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         mbinding.botonGuardar.setOnClickListener {
-            var capturaedit1=mbinding.ingresaEntero.text.toString().toInt()
-            spreference.edit().putInt("claveint",capturaedit1).apply()
+            if(mbinding.ingresaEntero.text.toString().isEmpty()){
+                Toast.makeText(this.activity,"Debe Ingresar Datos",Toast.LENGTH_SHORT).show()
+            }
+            else if (mbinding.ingesaTexto.text.toString().isEmpty()){
+                Toast.makeText(this.activity,"Debe Ingresar Datos",Toast.LENGTH_SHORT).show()
+            }
+            else if  (mbinding.ingresaDecimal.text.toString().isEmpty()){
+                Toast.makeText(this.activity,"Debe Ingresar Datos",Toast.LENGTH_SHORT).show()
+            }
+            else {
 
-            var capturaedit2=mbinding.ingesaTexto.text.toString()
-            spreference.edit().putString("clavetexto",capturaedit2).apply()
+                var capturaedit1 = mbinding.ingresaEntero.text.toString().toInt()
+                spreference.edit().putInt("claveint", capturaedit1).apply()
 
-            var capturaedit3=mbinding.ingresaDecimal.text.toString().toFloat()
-            spreference.edit().putFloat("clavedecimal",capturaedit3).apply()
+                var capturaedit2 = mbinding.ingesaTexto.text.toString()
+                spreference.edit().putString("clavetexto", capturaedit2).apply()
+
+                var capturaedit3 = mbinding.ingresaDecimal.text.toString().toFloat()
+                spreference.edit().putFloat("clavedecimal", capturaedit3).apply()
+            }
         }
         mbinding.textView.text=spreference.getInt("claveint",0).toString()
         mbinding.textView2.text=spreference.getString("clavetexto","x").toString()
